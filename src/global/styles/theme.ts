4b1d5aef0af4b7fabf068017d55a07cc60bdb057
LayanGabriel <layan.andrade14@gmail.com>
2025-05-30 20:55:32 -0400

    Fist Commit

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,46 @@
+
+
+export default{
+    colors: {
+        primary: '#be2edd',
+        secondary:'#f0932b',
+        background: '#F0F0F0',
+        error: '#B00200',
+
+        text: '#000000',
+        text_light: '#888888',
+
+        shape: '#ffffff',
+
+        attention: '#E83F5B',
+        attentionLight: '#FFCDD2',
+
+        success: '#4CAF50',
+        successLight: '#4CAF60'
+    },
+    fonts:{
+        regular:'Poppins_400Regular',
+        bold: 'Poppins_700Bold',
+        medium:'Poppins_500Medium',
+        black: 'Poppins_900Black',
+    },
+    spacing:{
+        small: 8,
+        medium:16,
+        large:24,
+        xlarge:32,
+    },
+    borderRadius:{
+        small:4,
+        medium:8,
+        large:16,
+        extralarge:23,
+    },
+    fontSize:{
+        small: 14,
+        medium: 18,
+        large: 22,
+        xlarge: 26,
+        xxlarge: 30,
+    }
+}
---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,56 @@
+import { HighlightCard } from "../../components/HighlightCard";
+import { 
+    Container, 
+    Header,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    UserWrapper,
+    Icon, 
+    HighlightCards,
+} from "./styles";
+
+
+export function Dashboard(){
+    return(
+        <Container>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                    <Photo source ={{uri:'https://img.freepik.com/fotos-gratis/cabeca-de-vaca-preta-e-branca-no-ceu-azul_268835-1008.jpg?semt=ais_hybrid&w=740'}}/>
+                    <User>
+                        <UserGreeting>Olá</UserGreeting>
+                        <UserName>Tia</UserName>
+                    </User>
+                    </UserInfo>
+                    <Icon name="power"/>
+                </UserWrapper>
+            </Header>
+            
+            <HighlightCards>
+                <HighlightCard 
+                    type = "up"
+                    title = "Entradas"
+                    amount = "R$ 17.000,00"
+                    lastTransaction = "Última entrada em 16 de junho."
+                />
+                <HighlightCard 
+                    type = "down"
+                    title = "Saída"
+                    amount = "R$ 12.000,00"
+                    lastTransaction = "Última saída em 10 de junho."
+                />
+                <HighlightCard 
+                    type = "total"
+                    title = "Total"
+                    amount = "R$ 7.000,00"
+                    lastTransaction = "De 1 a 16 de Junho"
+                />
+            </HighlightCards>
+
+        </Container>
+    );
+
+}
